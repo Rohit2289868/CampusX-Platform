975d51b5f4fce05ed8f6c3f8001e1eee83bab77b
Rohit2289868 <107972972+Rohit2289868@users.noreply.github.com>
2024-08-24 15:47:21 +0530

    Add files via upload
    Initialize the website with the list of 5 courses.

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header("CampusX")
+
+st.sidebar.title("Courses")
+ml = st.sidebar.button("Machine Learning")
+dl = st.sidebar.button("Deep Learning")
+aws = st.sidebar.button("AWS")
+ds = st.sidebar.button("Data Science")
+da = st.sidebar.button("Data Analytics")
+
+
